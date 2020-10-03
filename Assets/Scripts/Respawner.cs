b7e80cce00a2fb547824e8b7f6b58f7fb84d5990
DavidDieLandGurke <61883039+DavidDieLandGurke@users.noreply.github.com>
2020-10-03 11:16:09 +0200

    made respawning system

---
+++ Assets/Scripts/Respawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour
+{
+    public Transform spawnPos;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.transform.position = spawnPos.position;
+    }
+}
